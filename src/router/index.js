0b79f6f13d80f77ea40dc33b77e62849bea4a87b
ksweety <kahlilw03@gmail.com>
2023-05-31 13:32:10 -0400

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import AboutPage from '../views/AboutPage.vue'
 
 const routes = [
    {
       path: '/',
-      name: 'home',
       component: HomePage
    },
+   {
+    path: '/',
+    component: AboutPage
+ },
 ]
 
 const router = createRouter({
