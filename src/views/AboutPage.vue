894abd92f1ec7e6e1839a1a0b4d15506b2acbd9d
ksweety <kahlilw03@gmail.com>
2023-06-05 13:39:48 -0400

    ded

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,4 @@
+<template>
+Anime Scaling was made so that their would be no more questions on which anime character is stronger than the other. With this you will know for certain which anime character is stronger!!!!
+  </template>
+  
