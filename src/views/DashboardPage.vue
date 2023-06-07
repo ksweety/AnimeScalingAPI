0fbdb5e3e57b79bfd1cf5d95228ef5e31eb7b814
ksweety <kahlilw03@gmail.com>
2023-06-07 14:53:30 -0400

    fire commit

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,15 +1,41 @@
-<script>
-import { Ref } from 'vue';
+<script setup>
+import { ref } from 'vue';
 
 let userInput = ref("")
+let resultThing= ref()
+
+async function asyncFunction(){
+  
+}
+async function doAThing() {
+      const result = await fetch("https://nekos.best/api/v2/neko") 
+      console.log(result)
+      let json = await result.json();
+      resultThing.value =   json
+}
 </script>
 <template>
  <h1>
-  WELCOME TO ANIME SCALING!!! 
+ Waifu Generator 
+ <hr>
  </h1>
+ <h2>{{ resultThing?.results?.[0]?.artist_href}}</h2>
+ 
 
 
+<button @click="doAThing" class="dripbutton" type="submit">Click This For Waifu Link</button> 
+</template> 
 
-<v-text-field label="Ask your anime question"></v-text-field> 
-<v-btn v-model="userInput" type="submit">Submit</v-btn> 
-</template>
+<style>
+.dripbutton {
+  
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
